866b07094cb5eafad4ddea9b0e8966bf1ace88f9
Erfan <baradarantohidi@gmail.com>
2024-02-06 18:25:10 +0100

    finite amount of vesicle and delay in release of neurotransmitter

--- vesicle.py
+++ vesicle.py
@@ -13,26 +13,32 @@ def calcium_concentration(spike, tau_c, t_sim, dt):
     return calcium
 
 
-def vesicle_fusion( tau_f, t_sim, dt, v_0):
+def vesicle_fusion( tau_f, t_sim, dt, v_0, release_delay=0.1):
     release = np.zeros(int(t_sim/dt))
+    release_delay = int(release_delay/dt)
     
     fusion = np.zeros(int(t_sim/dt))
     for i in range(1, len(release)):
         fusion[i] = dt/tau_f * (v_0 - release[i-1]) + (1 - dt/tau_f) * fusion[i-1]
-        release[i] = calcium[i]* spike[i]* fusion[i]
-    return fusion
+        release[i] = calcium[i-release_delay]* spike[i-release_delay]* (v_0-fusion[i-release_delay])
+    return fusion, release
+
+rate = 10
+t_sim = 10
+dt = 0.001
+tau_c = 0.4
+tau_f = 0.3
+v_0 = 10
+
+spike = poisson_spike_train(rate, t_sim, dt)
+calcium = calcium_concentration(spike, tau_c, t_sim, dt)
+fusion, release = vesicle_fusion(tau_f, t_sim, dt, v_0)
 
 
-spike = poisson_spike_train(17, 1, 0.001)
-
-calcium = calcium_concentration(spike, 0.4, 1, 0.001)
-
-fusion = vesicle_fusion(0.1, 1, 0.001, 10)
-print(fusion)
-
-plt.plot(spike, label='Spike')
-plt.plot(calcium, label='Calcium')
-plt.plot(fusion, label='Fusion')
+plt.plot(spike, label='Spike', marker='o')
+plt.plot(calcium, label='Calcium concentration')
+plt.plot(fusion, label='Fusioned vesicle')
+plt.plot(release, label='Release')
 plt.xlabel('Time (ms)')
 plt.ylabel('Spike')
 plt.legend()
